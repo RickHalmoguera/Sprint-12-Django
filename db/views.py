bebad67deda404952aa5e075abdb4d8aef4dfcfc
Ricardo Halmoguera <halmoguerar@gmail.com>
2024-02-09 09:18:53 +0100

    ADD PAGINATION

--- db/views.py
+++ db/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from db.models import Booking
-from db.models import Contact
 from db.models import Room
 
 def create_booking(request, template_name='details.html'):
@@ -12,41 +12,21 @@ def create_booking(request, template_name='details.html'):
         except Exception as e:
             return render(request, template_name, {'error': str(e)})
 
-def contact_create(request, template_name='contact.html'):
-    if request.method == 'POST':
-        try:
-            contact = Contact(request.POST or None)
-            contact.save()
-            return render(request, template_name, {'data':contact})
-        except Exception as e:
-            return render(request, template_name, {'error': str(e)})
-    return render(request, 'contact.html')
+
 
 def rooms(request, template_name='rooms.html'):
     if request.method == 'GET':
         try:
-            rooms = Room.objects.all()
+            rooms_list = Room.objects.all()
+            paginator = Paginator(rooms_list, 10)  
+            page_number = request.GET.get('page')
+            rooms = paginator.get_page(page_number)
             return render(request, template_name, {'rooms': rooms})
         except Exception as e:
             return render(request, template_name, {'error': str(e)})
         
-def room(request, template_name='details.html'):
-    if request.method == 'GET':
-        try:
-            rooms = Room.objects.all()
-            available_rooms = [room for room in rooms if room.is_booked == True]
-            return render(request, template_name, {'rooms': available_rooms})
-        except Exception as e:
-            return render(request, template_name, {'error': str(e)})
-        
-def offers(request, template_name='offers.html'):
-    if request.method == 'GET':
-        try:
-            rooms = Room.objects.all()
-            available_rooms = [room for room in rooms if room.is_booked == True]
-            return render(request, template_name, {'available_rooms': available_rooms})
-        except Exception as e:
-            return render(request, template_name, {'error': str(e)})
+
+
         
 def index(request, template_name='index.html'):
     if request.method == 'GET':
